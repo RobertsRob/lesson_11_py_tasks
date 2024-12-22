bce2e08e26328a01be703d99766e2efbc3635930
RobertsRob <83211059+RobertsRob@users.noreply.github.com>
2024-12-22 19:03:38 +0200

    Update tasks_01.py

--- tasks_01.py
+++ tasks_01.py
@@ -15,8 +15,33 @@ print(book)
 
 
 # 2.task
+# Создайте класс Rectangle с аттрибутами длины и ширины. Реализуйте метод area, который вычисляет площадь прямоугольника.
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+    
+    def area(self):
+        return self.length * self.width
+
+rect = Rectangle(5, 3)
+print(rect.area())
+
 
 # 3.task
+# Создайте класс Student с аттрибутами: имя, возраст и список оценок. Реализуйте метод average_grade, который вычисляет средний балл.
+class Student:
+    def __init__(self, name, age, grades):
+        self.name = name
+        self.age = age
+        self.grades = grades
+    
+    def average_grade(self):
+        return sum(self.grades) / len(self.grades)
+
+student = Student("Ivan", 16, [4, 5, 4, 3, 5])
+print(student.average_grade())
+
 
 # 4.task
 
