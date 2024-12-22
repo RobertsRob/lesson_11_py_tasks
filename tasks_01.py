f04391adb5402b6418718183477be6a900672645
RobertsRob <83211059+RobertsRob@users.noreply.github.com>
2024-12-22 19:02:20 +0200

    Update tasks_01.py

--- tasks_01.py
+++ tasks_01.py
@@ -1,4 +1,18 @@
 # 1.task
+# Создайте класс Book с атрибутами: название, автор, год издания, цена. Реализуйте метод __str__, который выводит строковое представление объекта книги.
+class Book:
+    def __init__(self, title, author, year, price):
+        self.title = title
+        self.author = author
+        self.year = year
+        self.price = price
+    
+    def __str__(self):
+        return f'"{self.title}" автор - {self.author}, год - {self.year}, цена - {self.price}'
+
+book = Book("1984", "George Orwell", 1949, 500)
+print(book)
+
 
 # 2.task
 
