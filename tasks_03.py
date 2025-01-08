839eac3d8477526fc3823eaf529c8dfc16bddfb9
RobertsRob <83211059+RobertsRob@users.noreply.github.com>
2025-01-08 21:54:43 +0200

    Update tasks_03.py

--- tasks_03.py
+++ tasks_03.py
@@ -17,3 +17,44 @@
 # 9.task
 
 # 10.task
+
+
+
+/////////////////////////////////////////////////////// na dorabotku:
+
+# 3.task
+# Создайте класс Shape, в котором реализуйте метод area. Создайте класс Circle, который наследует от Shape и переопределяет метод area для вычисления площади круга.
+class Shape:
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+    
+    def area(self):
+        return 3.14 * self.radius ** 2
+
+circle = Circle(5)
+print(circle.area())
+
+# 4.task
+# Создайте класс Person, который имеет метод для изменения имени. Наследуйте от этого класса класс Employee, который добавляет аттрибут для зарплаты и переопределяет метод для изменения имени с добавлением приветствия.
+class Person:
+    def __init__(self, name):
+        self.name = name
+    
+    def change_name(self, new_name):
+        self.name = new_name
+
+class Employee(Person):
+    def __init__(self, name, salary):
+        super().__init__(name)
+        self.salary = salary
+    
+    def change_name(self, new_name):
+        super().change_name(new_name)
+        print(f"Привет, {self.name}!")
+
+emp = Employee("John", 5000)
+emp.change_name("Alex")
