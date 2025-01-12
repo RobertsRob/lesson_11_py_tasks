165d8de507297cac0fba96ee472e7111771f9189
RobertsRob <83211059+RobertsRob@users.noreply.github.com>
2025-01-12 17:49:46 +0200

    Update tasks_03.py

--- tasks_03.py
+++ tasks_03.py
@@ -1,6 +1,42 @@
 # 1.task
+# Создайте два класса: Shape и Rectangle. Класс Shape должен иметь метод area, который возвращает 0, а класс Rectangle должен наследовать Shape и переопределить метод area, чтобы вычислять площадь прямоугольника по формуле width * height.
+
+class Shape:
+    def area(self):
+        return 0
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+rectangle = Rectangle(5, 10)
+print(rectangle.area())  # 50
+
 
 # 2.task
+# Создайте два класса: Worker и Student. Класс Worker должен иметь метод work, который выводит сообщение о том, что работник выполняет свою работу. Класс Student должен иметь метод study, который выводит сообщение о том, что студент учится. 
+# Создайте класс WorkingStudent, который будет наследовать и от Worker, и от Student. Переопределите метод work так, чтобы он выводил и информацию о работе, и о учебе.
+
+class Worker:
+    def work(self):
+        print("Working on the job.")
+
+class Student:
+    def study(self):
+        print("Studying for exams.")
+
+class WorkingStudent(Worker, Student):
+    def work(self):
+        super().work()
+        self.study()
+
+working_student = WorkingStudent()
+working_student.work()
+
 
 # 3.task
 
