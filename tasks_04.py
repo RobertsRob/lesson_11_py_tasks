bafa5cab199a6ee3170030c0a0ef72270ee1e854
RobertsRob <83211059+RobertsRob@users.noreply.github.com>
2025-01-12 18:38:43 +0200

    Update tasks_04.py

--- tasks_04.py
+++ tasks_04.py
@@ -1,4 +1,29 @@
 # 1.task
+# Создайте абстрактный класс Shape с абстрактным методом area(). Затем создайте два класса-наследника: Circle и Rectangle, которые реализуют метод area(). 
+# Создайте список объектов разных классов и вызовите метод area() для каждого.
+
+class Shape():
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius**2
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+shapes = [Circle(5), Rectangle(4, 6)]
+for shape in shapes:
+    print(shape.area())
 
 # 2.task
 
